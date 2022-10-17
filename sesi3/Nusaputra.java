fb8681598a79486ee3956290608c647363d4af0f
Rufus8702 <Jamaludinahmad013@gmail.com>
2022-10-17 18:46:59 +0700

    yow

--- sesi3/Nusaputra.java
+++ sesi3/Nusaputra.java
@@ -36,6 +36,10 @@ public class Nusaputra extends Mahasiswa {
             System.out.println("===================================");
         }
 
+        //print tiap mahasiswa 
+        for(int i = 0; i < 4; i++){
+            mhs.get(i).infoMahasiswa();
+        }
         System.out.println("jumlah mahasiswa : " + mhs.size());
         System.out.print("jumlah mahasiswa yang lulus : ");
         printMhs(lulus);
